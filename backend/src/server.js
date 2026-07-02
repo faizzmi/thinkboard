ece603594493c991b0eb7f12d3d3e94afb4f29d2
faizzmi <faizzmi74@gmail.com>
2026-07-02 14:09:53 +0800

    fix: correct environment variable check in axios configuration and reorder middleware in server setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,19 +35,9 @@ app.use(morgan(
     },
 }));
 
-// simple applications
-// other application auth check ans rate limiting (req)
-// app.use((req, res, next) => {
-//     console.log(`req method is ${req.method} & Req URL is ${req.url}`);
-//     next();
-// })
-
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", notesRoutes);
 app.use("/api/logs", logsRoutes);
-
-app.use(notFoundHandler);
-app.use(errorHandler);
 
 process.on("uncaughtException", (err) => {
     logger.error("Uncaught Exception", { message: err.message, stack: err.stack });
@@ -66,6 +56,9 @@ if (process.env.NODE_ENV ==="production"){
         res.sendFile(path.join(__dirname, "../frontend/dist","index.html"))
     })
 };
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" 
+const BASE_URL = import.meta.env.NODE_ENV === "development" 
   ? "http://localhost:5001" 
   : "";
 
