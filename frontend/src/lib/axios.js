49f946c8a0010f40015106bf4da0c6f26cc2c37c
faizzmi <faizzmi74@gmail.com>
2026-07-05 13:43:29 +0800

    fix: remove duplicate url for backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.NODE_ENV === "development" 
   ? "http://localhost:5001" 
-  : "/api";
+  : "";
 
 const api = axios.create({
     baseURL: BASE_URL,
